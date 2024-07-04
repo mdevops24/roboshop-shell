8b9aea17c7ccb4680d7f8b345d12ef8f6defa092
mohan <mohanreddy0532@gmail.com>
2024-07-04 11:19:06 +0530

    user sh

---
+++ user.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cd /app
+unzip /tmp/user.zip
+
+
+
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
