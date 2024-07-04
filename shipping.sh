1b3cdd93e26414cfe415f55d6ee24824a7a077f6
mohan <mohanreddy0532@gmail.com>
2024-07-04 15:42:58 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -15,7 +15,6 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
 dnf install mysql -y
-
 mysql -h mysql.dev.mdevops24.online -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql.dev.mdevops24.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 mysql -h mysql.dev.mdevops24.online -uroot -pRoboShop@1 < /app/db/app-user.sql
