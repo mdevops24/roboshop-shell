d52a114cc6b52b79721391c2e1717e089e57c7d3
mohan <mohanreddy0532@gmail.com>
2024-07-04 16:41:10 +0530

    update dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -1,7 +1,8 @@
 cp dispatch.service /etc/systemd/system/dispatch.service
 
 dnf install golang -y
-
+useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
