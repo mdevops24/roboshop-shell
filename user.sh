9541bc458c17b6dc8340d533b1a139a61a9a8026
mohan <mohanreddy0532@gmail.com>
2024-07-04 15:26:55 +0530

    user update

--- user.sh
+++ user.sh
@@ -11,9 +11,8 @@ mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app
 unzip /tmp/user.zip
-
-
-
+cd /app
+npm install
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
