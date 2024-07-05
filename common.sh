70b35504b730d5811937ed4502d503005a691c41
mohan <mohanreddy0532@gmail.com>
2024-07-05 19:18:42 +0530

    dry code

--- common.sh
+++ common.sh
@@ -1,40 +1,47 @@
 LOG_FILE=/tmp/roboshop.log
+rm -rf $LOG_FILE
+
+PRINT(){
+  echo
+  echo
+  echo ================$*==================
+}
 
 NODEJS(){
-  echo Disable NodeJS default version
+  PRINT Disable NodeJS default version
   dnf module disable nodejs -y &>>$LOG_FILE
 
-  echo Enable NodeJS 20 module
+  PRINT Enable NodeJS 20 module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
 
-  echo Install NodeJS
+  PRINT Install NodeJS
   dnf install nodejs -y &>>$LOG_FILE
 
-  echo Cpoy Service file
+  PRINT Cpoy Service file
   cp ${component}.service /etc/systemd/system/${component}.service  &>>$LOG_FILE
 
-  echo Copy MongoDB repo file
+  PRINT Copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo  &>>$LOG_FILE
 
-  echo Add Application User
+  PRINT Add Application User
   useradd roboshop
   rm -rf /app  &>>$LOG_FILE
 
-  echo Make /app directory
+  PRINT Make /app directory
   mkdir /app  &>>$LOG_FILE
 
-  echo Create App content
+  PRINT Create App content
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip  &>>$LOG_FILE
 
   cd /app
-  echo Extract App content
+  PRINT Extract App content
   unzip /tmp/${component}.zip  &>>$LOG_FILE
   cd /app
 
-  echo Install NodeJS npm dependencies
+  PRINT Install NodeJS npm dependencies
   npm install  &>>$LOG_FILE
 
-  echo Start Service
+  PRINT Start Service
   systemctl daemon-reload  &>>$LOG_FILE
   systemctl enable ${component}  &>>$LOG_FILE
   systemctl start ${component}   &>>$LOG_FILE
