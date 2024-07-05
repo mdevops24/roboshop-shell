fa204d20cb6b76b6e5c390a9fccefbbf03767a21
mohan <mohanreddy0532@gmail.com>
2024-07-05 16:56:20 +0530

    dry code

--- user.sh
+++ user.sh
@@ -1,17 +1,6 @@
 source common.sh
 
-cp user.service /etc/systemd/system/user.service
 
 NODEJS
 
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+
