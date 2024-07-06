6088e145825bb5fe3716b7230ffdd8073f1aae21
mohan <mohanreddy0532@gmail.com>
2024-07-06 17:39:23 +0530

    dry code exit status

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,10 @@ source common.sh
 component=catalogue
 NODEJS
 
+PRINT Install mongodb client
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+echo $?
 
-dnf install mongodb-mongosh -y
-mongosh --host mongo.dev.mdevops24.online </app/db/master-data.js
+PRINT Load master data
+mongosh --host mongo.dev.mdevops24.online </app/db/master-data.js &>>$LOG_FIL
+echo $?
