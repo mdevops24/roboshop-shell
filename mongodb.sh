0fd9d83fddbce06013c576bf929dc262f3cee134
mohan <mohanreddy0532@gmail.com>
2024-07-07 16:16:33 +0530

    dry code updates redis,mongo

--- mongodb.sh
+++ mongodb.sh
@@ -2,18 +2,18 @@ source common.sh
 component=mongo
 
 PRINT Copy MongoDB repo
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>>$LOG_FILE
 STAT $?
 
 PRINT Install mongoDB
-dnf install mongodb-org -y
+dnf install mongodb-org -y  &>>$LOG_FILE
 STAT $?
 
 PRINT Update MongoDB Conf file
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>$LOG_FILE
 STAT $?
 
 PRINT Enable and Start Mongo DB Servie
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod  &>>$LOG_FILE
+systemctl restart mongod  &>>$LOG_FILE
 STAT $?
--- redis.sh
+++ redis.sh
@@ -2,22 +2,22 @@ source common.sh
 component=redis
 
 PRINT Disable old redis
-dnf module disable redis -y
+dnf module disable redis -y  &>>$LOG_FILE
 STAT $?
 
 PRINT Enable redis 7
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y  &>>$LOG_FILE
 STAT $?
 
 PRINT Install redis
-dnf install redis -y
+dnf install redis -y  &>>$LOG_FILE
 STAT $?
 
 PRINT Edit redis.conf to 0000 and preotected-mode
-sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf -e '/protected-mode/ c protected-mode no' #to replace to 0.0.0.0
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf -e '/protected-mode/ c protected-mode no' &>>$LOG_FILE #to replace to 0.0.0.0  &>>$LOG_FILE
 STAT $?
 
 PRINT Enable and start redis
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis  &>>$LOG_FILE
+systemctl restart redis &>>$LOG_FILE
 STAT $?
