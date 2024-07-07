39dd1bbbec610743eef7587666b14fbfb0b6958a
mohan <mohanreddy0532@gmail.com>
2024-07-07 16:14:22 +0530

    dry code updates redis

--- redis.sh
+++ redis.sh
@@ -1,9 +1,23 @@
+source common.sh
+component=redis
+
+PRINT Disable old redis
 dnf module disable redis -y
+STAT $?
+
+PRINT Enable redis 7
 dnf module enable redis:7 -y
+STAT $?
+
+PRINT Install redis
 dnf install redis -y
+STAT $?
 
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf #to replace to 0.0.0.0
-sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+PRINT Edit redis.conf to 0000 and preotected-mode
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf -e '/protected-mode/ c protected-mode no' #to replace to 0.0.0.0
+STAT $?
 
+PRINT Enable and start redis
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
+STAT $?
