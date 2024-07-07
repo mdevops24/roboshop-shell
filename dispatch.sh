02a097bee417790d5553f495f7acd3ac70a67965
mohan <mohanreddy0532@gmail.com>
2024-07-07 19:13:59 +0530

    dry code updates in common fr dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -1,18 +1,17 @@
-cp dispatch.service /etc/systemd/system/dispatch.service
+source common.sh
+component=payment
+app_path=/app
 
-dnf install golang -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
-unzip /tmp/dispatch.zip
+PRINT Install GoLang
+dnf install golang -y &>>$LOG_FILE
+STAT $?
 
-cd /app
-go mod init dispatch
-go get
-go build
+APP_PREREQ
 
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+PRINT download the dependencies & build the software
+go mod init dispatch  &>>$LOG_FILE
+go get                &>>$LOG_FILE
+go build              &>>$LOG_FILE
+STAT $?
+
+SYSTEMD_SETUP
