7abefe1b556ed5ac52be9083875eff085625c1ba
mohan <mohanreddy0532@gmail.com>
2024-07-07 16:00:43 +0530

    dry code updates Mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,19 @@
+source common.sh
+component=mongo
+
+PRINT Copy MongoDB repo
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+STAT $?
+
+PRINT Install mongoDB
 dnf install mongodb-org -y
+STAT $?
+
+PRINT Update MongoDB Conf file
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+PRINT Enable and Start Mongo DB Servie
 systemctl enable mongod
 systemctl restart mongod
+STAT $?
