5e274c1c4574146f978ddd482aa925ad06969301
mohan <mohanreddy0532@gmail.com>
2024-07-07 16:21:54 +0530

    dry code updates mysql

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+source common.sh
+component=mysql
+
+PRINT Install mysql server
 dnf install mysql-server -y
+STAT $?
 
+PRINT Enable mysqld and start
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+STAT $?
 
+PRINT Setup MySQL root pwd
 mysql_secure_installation --set-root-pass RoboShop@1
+STAT $?
