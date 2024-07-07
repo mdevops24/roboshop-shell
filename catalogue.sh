448a6a4662cd2b19b063a44ca8574979a5d4b0cb
mohan <mohanreddy0532@gmail.com>
2024-07-07 16:03:20 +0530

    dry code updates Mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 STAT $?
 
 PRINT Load master data
-mongosh --host mongo.dev.mdevops24.online </app/db/master-data.js &>>$LOG_FILE
+#mongosh --host mongo.dev.mdevops24.online </app/db/master-data.js &>>$LOG_FILE
+mongosh --host localhost </app/db/master-data.js &>>$LOG_FILE
 STAT $?
 
