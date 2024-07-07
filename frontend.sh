1ce3d97ac642336b12f8b6c1f25c78f9743d159f
mohan <mohanreddy0532@gmail.com>
2024-07-07 14:12:31 +0530

    dry code updates user id

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,36 @@
-cp nginx.conf /etc/nginx/nginx.conf
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+source common.sh
+component=frontend
+
+PRINT Disable Nginx default version
+dnf module disable nginx -y &>>$LOG_FILE
+STAT $?
+
+PRINT Enable Nginx 24 version
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+STAT $?
+
+PRINT Install Nginx
+dnf install nginx -y &>>$LOG_FILE
+STAT $?
+
+PRINT Copy nginx.conf file
+cp nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+STAT $?
+
+PRINT Remove old contenet
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+STAT $?
+
+PRINT Download Application contenet
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+STAT $?
+
+PRINT Extract Application content
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx
+unzip /tmp/frontend.zip &>>$LOG_FILE
+STAT $?
+
+PRINT Enable,Start Nginx
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
+STAT $?
