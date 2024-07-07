176ebb96701fe92aed3b4c25f27abfde8e1dc575
mohan <mohanreddy0532@gmail.com>
2024-07-07 16:44:48 +0530

    dry code updates shipping

--- shipping.sh
+++ shipping.sh
@@ -1,28 +1,5 @@
-cp shipping.service /etc/systemd/system/shipping.service
+source common.sh
+component=shipping
+app_path=/app
 
-dnf install maven -y
-
-useradd roboshop
-rm -rf /app
-mkdir /app
-
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
-cd /app
-unzip /tmp/shipping.zip
-
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-dnf install mysql -y
-mysql -h mysql.dev.mdevops24.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql.dev.mdevops24.online -uroot -pRoboShop@1 < /app/db/master-data.sql
-mysql -h mysql.dev.mdevops24.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-
-
-
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
-
-
+JAVA
